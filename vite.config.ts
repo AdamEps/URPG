0db5960873aa3cb47185ad23f7917c77809cff50
AdamEps <your-email@example.com>
2025-08-21 23:07:21 -0400

    Update for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/urpg/', // This should match your GitHub repository name
+  base: '/URPG/', // This should match your GitHub repository name
   server: {
     port: 3000,
     host: true
